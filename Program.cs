9f69e0d6cf880a5d3438ee8eec3bc6fb6a570bb8
kamin0 <jm187@yandex.ru>
2022-05-09 12:53:38 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace fileSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public class Drive
+    {
+        public Drive(string name, long totalSpace, long freeSpace)
+        {
+            Name = name;
+            TotalSpace = totalSpace;
+            FreeSpace = freeSpace;
+        }
+
+        public string Name { get; }
+        public long TotalSpace { get; }
+        public long FreeSpace { get; }
+    }
+}
