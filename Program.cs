c9b4d2c781c9c07425c488f2f1369aa69b71fd39
kamin0 <jm187@yandex.ru>
2022-05-09 14:17:11 +0300

    8.2.4

--- Program.cs
+++ Program.cs
@@ -11,14 +11,17 @@ namespace fileSystem
         {
             CreateCatalog();
             GetCatalogs(); //   Вызов метода получения директорий
-            
+            //CatalogMove();
+            //CatalogDelete();
+            Trash();
+
         }
 
         static void GetCatalogs()
         {
             try
             {
-                DirectoryInfo dirInfo = new DirectoryInfo(@"D:\\" /* Или С:\\ для Windows */ );
+                DirectoryInfo dirInfo = new DirectoryInfo(@"C:\Users\kamin0\Desktop\" /* Или С:\\ для Windows */ );
                 if (dirInfo.Exists)
                 {
                     Console.WriteLine(dirInfo.GetDirectories().Length + dirInfo.GetFiles().Length);
@@ -31,13 +34,48 @@ namespace fileSystem
         }
         static void CreateCatalog()
         {
-            DirectoryInfo dirInfo = new DirectoryInfo(@"D:\\2\");
+            DirectoryInfo dirInfo = new DirectoryInfo(@"C:\Users\kamin0\Desktop\");
             if (!dirInfo.Exists)
                 dirInfo.Create();
 
             dirInfo.CreateSubdirectory("NewFolder");
+        }
+
+        //static void CatalogDelete()
+        //{
+        //   try
+        //    {
+         //       DirectoryInfo dirInfo = new DirectoryInfo(@"D:\\2\");
+          //      dirInfo.Delete(true); // Удаление со всем содержимым
+           //     Console.WriteLine("Каталог удален");
+           // }
+           // catch (Exception ex)
+           // {
+           //     Console.WriteLine(ex.Message);
+           /// }
+        //}
+        static void CatalogMove()
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(@"C:\Users\kamin0\Desktop\NewFolder");
+            string newPath = @"C:\$RECYCLE.BIN\NewFolder";
+
+            if (dirInfo.Exists && Directory.Exists(@"C:\Users\kamin0\Desktop\NewFolder")) ;
+            dirInfo.MoveTo(newPath);
+
+        }
+        static void Trash()
+        {
+            try
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo(@"C:\Users\kamin0\Desktop\NewFolder");
+                string trashPath = @"C:\$RECYCLE.BIN\NewFolder";
+
+                dirInfo.MoveTo(trashPath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
             }
         }
-        
     }
-
+}
