e4e535fb99d3f40147435558d5e0530a5a45a2f6
kamin0 <jm187@yandex.ru>
2022-05-09 16:01:57 +0300

    8.3 размер папок

---
+++ DirectoryExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectoryE
+{
+    public static class DirectoryExtension
+    {
+        public static long DirSize(DirectoryInfo d)
+        {
+            long size = 0;
+            FileInfo[] fis = d.GetFiles();
+            foreach (FileInfo fi in fis)
+            {
+                size += fi.Length;
+            }
+            DirectoryInfo[] dis = d.GetDirectories();
+            foreach (DirectoryInfo di in dis)
+            {
+                size += DirSize(di);
+            }
+            return size;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,34 +1,56 @@
 ﻿using System;
 using System.IO;
-class FileWriter
-{
-    public static void Main()
-    {
-        string filePath = @"C:\Users\kamin0\source\repos\fileSystem\Program.cs"; // Укажем путь 
-        //if (!File.Exists(filePath)) // Проверим, существует ли файл по данному пути
-        //{
-            //   Если не существует - создаём и записываем в строку
-           // using (StreamWriter sw = File.CreateText(filePath))  // Конструкция Using (будет рассмотрена в последующих юнитах)
-            //{
-             //   sw.WriteLine("Олег");
-            //    sw.WriteLine("Дмитрий");
-           //     sw.WriteLine("Иван");
-           // }
-       // }
-        // Откроем файл и прочитаем его содержимое
+using System.Linq;
+using DirectoryE;
 
-        using (StreamReader sr = File.OpenText(filePath))
+namespace DriveManager
+{
+    class Program
+    {
+        static void Main(string[] args)
         {
-            string str = "";
-            while ((str = sr.ReadLine()) != null) // Пока не кончатся строки - считываем из файла по одной и выводим в консоль
+            // получим системные диски
+            DriveInfo[] drives = DriveInfo.GetDrives();
+
+            // Пробежимся по дискам и выведем их свойства
+            foreach (DriveInfo drive in drives.Where(d => d.DriveType == DriveType.Fixed))
             {
-                Console.WriteLine(str);
+                WtiteDriveInfo(drive);
+
+                DirectoryInfo root = drive.RootDirectory;
+                var folders = root.GetDirectories();
+                WtiteFolderInfo(folders);
             }
         }
-        using (StreamWriter sw = File.AppendText(filePath))
+
+        public static void WtiteDriveInfo(DriveInfo drive)
         {
-            sw.WriteLine("//Последний раз программа запускалась: {0}", DateTime.Now);
+            Console.WriteLine($"Название: {drive.Name}");
+            Console.WriteLine($"Тип: {drive.DriveType}");
+            if (drive.IsReady)
+            {
+                Console.WriteLine($"Объем: {drive.TotalSize}");
+                Console.WriteLine($"Свободно: {drive.TotalFreeSpace}");
+                Console.WriteLine($"Метка: {drive.VolumeLabel}");
+            }
         }
+        public static void WtiteFolderInfo(DirectoryInfo[] folders)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Папки: ");
+            Console.WriteLine();
+            foreach (var folder in folders)
+            {
+                try
+                {
+                    Console.WriteLine(folder.Name + "- {0} байт", DirectoryExtension.DirSize(folder));
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(folder.Name + "Не удалось рассчитать размер - ошибка: {0}", e.Message);
+                }
+            }
+        }
+
     }
-}//Последний раз программа запускалась: 09.05.2022 15:32:00
-//Последний раз программа запускалась: 09.05.2022 15:32:16
+}
